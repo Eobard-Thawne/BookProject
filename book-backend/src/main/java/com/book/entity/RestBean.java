0b37f419fa6b00965928fa446b2b2300820db160
truth <2488292668@qq.com>
2023-07-11 19:34:59 +0800

    RestBean创建

--- book-backend/src/main/java/com/book/entity/RestBean.java
+++ book-backend/src/main/java/com/book/entity/RestBean.java
@@ -1,2 +1,30 @@
-package com.book.entity;public class RestBean {
+package com.book.entity;
+
+import lombok.Data;
+
+@Data
+public class RestBean<T> {
+    private int status;
+    private boolean success;
+    T msg;
+
+    private RestBean(int status, boolean success, T msg) {
+        this.status = status;
+        this.success = success;
+        this.msg = msg;
+    }
+
+    public static <T> RestBean<T> success(int status, T msg) {
+        return new RestBean<>(status, true, msg);
+    }
+    public static <T> RestBean<T> failure(int status, T msg) {
+        return new RestBean<>(status, false, msg);
+    }
+
+
+    public static <T> RestBean<T> success(T msg) {
+        return new RestBean<>(200, true, msg);
+
+    }
+
 }
