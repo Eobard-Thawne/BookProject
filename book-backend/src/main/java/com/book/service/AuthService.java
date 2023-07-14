fb19d604df7861341a4d6e816cbc2e056b8b6480
truth <2488292668@qq.com>
2023-07-15 00:49:02 +0800

    用户认证服务

--- book-backend/src/main/java/com/book/service/AuthService.java
+++ book-backend/src/main/java/com/book/service/AuthService.java
@@ -1,11 +1,27 @@
 package com.book.service;
 
+import com.book.entity.Account;
+import com.book.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public interface AuthService extends UserDetailsService {
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Resource
+    UserMapper userMapper;
 
     @Override
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = userMapper.findUserByUserName(username);
+        return User
+                .withUsername(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
 }
