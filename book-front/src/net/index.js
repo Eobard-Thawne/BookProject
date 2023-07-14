7c8063acf3f7c312d250bec47577167cf5550929
truth <2488292668@qq.com>
2023-07-15 00:52:31 +0800

    axios封装

---
+++ book-front/src/net/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import {ElMessage} from "element-plus";
+
+const defaultError = () => ElMessage.error("发生了一些错误")
+const defaultFailure = (msg) => ElMessage.warning(msg)
+
+const BASE_URL = "http://localhost:8080"
+
+function post(url, data, success, failure = defaultFailure, error = defaultError) {
+    axios.post(BASE_URL + url, data, {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        withCredentials: true
+    }).then(({data}) => {
+        if (data.success) {
+            success(data.msg, data.status)
+        } else {
+            failure(data.msg, data.status)
+        }
+    }).catch(error)
+}
+
+function get(url, success, failure = defaultFailure, error = defaultError) {
+    axios.get(url, {
+        withCredentials: true
+    }).then(({data}) => {
+        if (data.success) {
+            success(data.msg, data.status)
+        } else {
+            failure(data.msg, data.status)
+        }
+    }).catch(error)
+}
+
+export {get, post}
