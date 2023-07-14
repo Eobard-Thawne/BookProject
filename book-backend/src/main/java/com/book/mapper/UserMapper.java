19fe972a363ef17a35cb2cbcf7cd0bfda066b5d5
truth <2488292668@qq.com>
2023-07-15 00:48:42 +0800

    dao层用户查询接口

--- book-backend/src/main/java/com/book/mapper/UserMapper.java
+++ book-backend/src/main/java/com/book/mapper/UserMapper.java
@@ -1,7 +1,14 @@
 package com.book.mapper;
 
+import com.book.entity.Account;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
+    @Select("select * from user where username = #{username}")
+    Account findUserByUserName(@Param("username") String username);
+
 }
