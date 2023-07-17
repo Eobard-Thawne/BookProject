9b5d31f5097c635d06b87fcc47242210721acbd8
truth <2488292668@qq.com>
2023-07-17 22:03:43 +0800

    数据库实体类重写

--- book-backend/src/main/java/com/book/entity/Account.java
+++ book-backend/src/main/java/com/book/entity/Account.java
@@ -2,13 +2,10 @@ package com.book.entity;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class Account {
     Integer id;
     String username;
     String password;
     String role;
-    Date createDate;
 }
