6871ab0ee2201e9a1ba5e3cc1fae98c42b9d31a2
truth <2488292668@qq.com>
2023-07-17 22:05:15 +0800

    子路由配置

--- book-front/src/main.js
+++ book-front/src/main.js
@@ -1,7 +1,14 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import router from "@/router";
+import router from "@/router/router";
+import "element-plus/dist/index.css"
+import ElementPlus from "element-plus"
+import * as ElementIcons from '@element-plus/icons-vue'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementIcons)) {
+    app.component(key, component)
+}
 app.mount('#app')
--- book-front/src/router/router.js
+++ book-front/src/router/router.js
@@ -11,6 +11,11 @@ const router = createRouter({
                     path: '',
                     name: 'login',
                     component: import('@/views/LoginPage.vue')
+                },
+                {
+                    path: 'register',
+                    name: 'register',
+                    component: import('@/views/RegisterPage.vue')
                 }
             ]
         },
