830822e2b80747133f264bd692fd67caff17cf51
truth <2488292668@qq.com>
2023-07-17 22:05:35 +0800

    主页面修改

--- book-front/src/views/IndexPage.vue
+++ book-front/src/views/IndexPage.vue
@@ -1,8 +1,16 @@
 <template>
   <h1>这是主页</h1>
+  <el-button @click="logout()">退出登录</el-button>
 </template>
 
-<script>
+<script setup>
+import {get} from "@/net";
+import {ElMessage} from "element-plus";
+function logout() {
+    get("/api/auth/logout", (msg)=>{
+        ElMessage.success(msg)
+    })
+}
 </script>
 
 <style>
