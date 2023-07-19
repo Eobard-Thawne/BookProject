3821ca7a4c2bd8ea8c4e74edba2a53b922a2f21e
truth <2488292668@qq.com>
2023-07-19 20:54:56 +0800

    主页

---
+++ book-front/src/components/MainPage.vue
@@ -0,0 +1,43 @@
+<script>
+</script>
+<template>
+    <div class="container">
+        <div style="flex: 1">
+            <el-image class="bg" :src="bg" fit="cover"/>
+        </div>
+        <div class="login">
+            <transition name="el-zoom-in-center">
+                <router-view/>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import bg from "@/assets/bg.jpg";
+</script>
+
+<style>
+.container {
+    overflow: hidden;
+    display: flex;
+    width: 100vw;
+    height: 100vh
+}
+
+.bg {
+    width: 100%;
+    height: 100%
+}
+
+.login {
+    width: 500px;
+    background-color: white;
+    text-align: center;
+    font-size: 30px;
+}
+
+.loginForm {
+    margin: 0 100px;
+}
+</style>
